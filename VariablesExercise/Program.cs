d98c84db624116915a066db9472b629d2722fe31
J-Norse <jgb.norse@gmail.com>
2026-01-18 14:05:58 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "JNorse";
+            int age = 33;
+            char initial = 'J';
+            bool isHappy = true;
+            double avg = 99.9;
+            decimal savingsAccount = 500.00m;
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nmyinitial: {initial}\nHappy: {isHappy}\nAvg: {avg}\nAccount {savingsAccount}");
         }
     }
 }
